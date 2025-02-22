8e01689ed598b35e3c4855fed58a44fe45780f1d
Hantq04 <hantq04@gmail.com>
2025-02-23 00:07:15 +0700

    initial commit

--- src/main/java/com/example/exceptiondemo/config/security/LogoutService.java
+++ src/main/java/com/example/exceptiondemo/config/security/LogoutService.java
@@ -1,5 +1,6 @@
 package com.example.exceptiondemo.config.security;
 
+import com.example.exceptiondemo.controller.UserController;
 import com.example.exceptiondemo.repository.TokenRepo;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,19 +9,30 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
+    private static final Logger logger = Logger.getLogger(LogoutService.class.getName());
     private final TokenRepo tokenRepo;
 
     @Override
     public void logout(HttpServletRequest request,
                        HttpServletResponse response,
                        Authentication authentication) {
+        logger.info("-------- Logout --------");
         final var authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             return;
         }
         var jwt = authHeader.substring(7);
+        var storedToken = tokenRepo.findByToken(jwt)
+                .orElse(null);
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepo.save(storedToken);
+        }
     }
 }
