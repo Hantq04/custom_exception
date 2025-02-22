092ec9dba19a5cbc0c31577b059bfdd849769882
Hantq04 <hantq04@gmail.com>
2025-02-22 23:55:45 +0700

    initial commit

--- src/main/java/com/example/exceptiondemo/config/security/LogoutService.java
+++ src/main/java/com/example/exceptiondemo/config/security/LogoutService.java
@@ -1,11 +1,18 @@
 package com.example.exceptiondemo.config.security;
 
+import com.example.exceptiondemo.repository.TokenRepo;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
+    private final TokenRepo tokenRepo;
+
     @Override
     public void logout(HttpServletRequest request,
                        HttpServletResponse response,
